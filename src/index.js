e09632049fd99af9922e80948719bd64e49a4732
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-10-15 20:42:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/tweets-user-cards'>
+    <BrowserRouter basename='/tweets-cards-project'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
